5ef33395ad327302498ea994e48e5317a98c5668
Alex Booker <alexbooker@fastmail.im>
2017-02-06 16:52:45 +0000

    Add `User` Sequelize model

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import * as Hapi from "hapi"
 import * as opn from "opn"
+import * as Sequelize from "sequelize"
 
 const server = new Hapi.Server()
 const port = 3000
@@ -19,8 +20,38 @@ server.start(error => {
   if (error) {
     console.error(error)
   } else {
-    const address = `http://localhost:${port}` 
+    const address = `http://localhost:${port}`
     console.log(`🚀 Server running ${address}`)
     opn(address)
   }
+})
+
+const connection = new Sequelize("apollo_schema", "root", "")
+
+interface UserAttributes {
+  username: string,
+  password: string
+}
+
+interface UserInstance extends Sequelize.Instance<UserAttributes> {
+  dataValues: UserAttributes
+}
+
+const User = connection.define<UserInstance, UserAttributes>("user", {
+  username: {
+    type: Sequelize.STRING
+  },
+  password: { 
+    type: Sequelize.TEXT
+  }
+})
+
+connection.sync({
+  force: true
+}).then(function () {
+  return User.create({
+    username: "username",
+    password: "password"
+  })
+}).then(function (user) {
 })
